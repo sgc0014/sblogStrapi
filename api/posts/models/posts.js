4dac29d5ea4128e6c62b1b281881a35ab56a38cd
unknown <girisagar0014@gmail.com>
2020-09-22 11:22:43 +0545

    final final

---
+++ api/posts/models/posts.js
@@ -0,0 +1,6 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
+ * to customize this model
+ */
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f83602ad-7a07-4a87-b667-dccc9fb849d0'
+};
